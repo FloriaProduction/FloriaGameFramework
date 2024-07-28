3d1aad3a6d2ec0f1290014a7baadeed1843571ec
FloriaProduction <FloriaProduction@yandex.ru>
2024-07-29 00:10:49 +0700

    upd m

---
+++ FloriaGF/Functions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FloriaGF
+{
+    public static class Func
+    {
+        public static float smooth(float a, float b, float x, float min_dif = 0)
+        {
+            float result = a + (b - a) * x;
+            if (Math.Abs(b - result) <= min_dif)
+                return b;
+            return result;
+        }
+    }
+}
